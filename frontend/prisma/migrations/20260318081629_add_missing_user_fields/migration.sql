30ffd62cd0e3699914c3deafc6ac9050db59d9be
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2026-04-10 16:46:13 +0500

    chore: remove obsolete migration files
    - Deleted migration lock file and SQL migration scripts that are no longer needed, streamlining the migration process and reducing clutter in the project.

--- frontend/prisma/migrations/20260318081629_add_missing_user_fields/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "bridge_activities" ADD COLUMN     "clientIp" TEXT,
-ADD COLUMN     "epoch" INTEGER;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "lastLoginAt" TIMESTAMP(3),
-ADD COLUMN     "lastLoginIp" TEXT;
